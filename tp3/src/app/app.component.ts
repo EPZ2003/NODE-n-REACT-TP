8a476bc46513bfdcb5c43335c3d9187c79c32028
Enzo Polizzi <enzo.polizzi.11@gmail.com>
2024-10-22 14:38:45 +0200

    STEP 3-4

--- tp3/src/app/app.component.ts
+++ tp3/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'tp3';
+  constructor(){
+    console.log('AppComponent.constructror');
+  }
+  ngOnInit(): void {
+    console.log("AppCompnent.ngOnInit()")
+  }
 }
